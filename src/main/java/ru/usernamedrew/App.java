e2fae524e621951ee671c417651a8615f0a9ec2b
Андрей Бурлачко <andrejburlacko@MacBook-Air-Andrej.local>
2025-03-13 23:49:17 +0300

    Added basic functions

--- src/main/java/ru/usernamedrew/App.java
+++ src/main/java/ru/usernamedrew/App.java
@@ -3,7 +3,7 @@ package ru.usernamedrew;
 
 public class App {
     public static void main( String[] args ) {
-        Note note = new Note("Title", "Hello, this is my note!");
-        System.out.println(note.getTitle());
+        NotesManager notesManager = new NotesManager();
+        notesManager.run();
     }
 }
--- src/main/java/ru/usernamedrew/NotesManager.java
+++ src/main/java/ru/usernamedrew/NotesManager.java
@@ -1,22 +1,120 @@
 package ru.usernamedrew;
 
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
-public class NotesManager {
-    private List<Note> notes;
-    private static final String FILE_NAME = "notes";
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
 
-    public NotesManager() {}
+enum Command {
+    Add, Edit, Delete, Get, Exit;
+}
 
-    private void addNote(Note note) {}
+@Slf4j
+public final class NotesManager {
+    private HashMap<Integer, Note> notes;
+    private int id = 0;
+    private static final String FILE_NAME = "notes.dat";
 
-    private void deleteNote(Note note) {}
+    public NotesManager() {
+        notes = loadNotes();
+        if (notes.isEmpty()) {
+            notes.put(id++ ,new Note("Title", "Hello, this is my note!"));
+        }
+    }
 
-    private void editNote(Note note) {}
+    /**
+     * The method in which the action is selected
+     */
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        Command cmd;
 
-    private void saveNotes() {}
+        do {
+            System.out.print("Enter command (Add, Edit, Delete, Get, Exit): ");
+            cmd = Command.valueOf(scanner.nextLine().toUpperCase());
+            switch (cmd) {
+                case Add -> addNote(scanner);
+                case Edit -> editNote(scanner);
+                case Delete -> deleteNote(scanner);
+                case Get -> printNotes();
+                case Exit -> saveNotes();
+                default -> {
+                    System.out.println("Invalid command");
+                    log.warn("Invalid command");
+                }
+            }
+        } while (cmd != Command.Exit);
+    }
 
-    private List<Note> loadNotes() {
+    /**
+     * Method that implements adding the note
+     * @param scanner input stream
+     */
+    private void addNote(Scanner scanner) {
+        System.out.print("Enter the title of the note: ");
+        String title = scanner.nextLine();
+        System.out.print("Enter the content of the note: ");
+        String content = scanner.nextLine();
+        notes.put(id++, new Note(title, content));
+        log.info("Note added: " + id--);
+    }
+
+    /**
+     * Method that implements deleting the note
+     * @param scanner input stream
+     */
+    private void deleteNote(Scanner scanner) {
+        System.out.print("Enter the number of the note: ");
+        int noteNumber = Integer.parseInt(scanner.nextLine());
+        notes.remove(noteNumber);
+        log.info("Note deleted: " + noteNumber);
+    }
+
+    /**
+     * Method that implements editing the note
+     * @param scanner input stream
+     */
+    private void editNote(Scanner scanner) {
+        System.out.print("Enter the number of the note: ");
+        int noteNumber = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter the new content of the note: ");
+        String content = scanner.nextLine();
+        notes.get(noteNumber).setContent(content);
+        log.info("Note updated: " + noteNumber);
+    }
+
+    /**
+     * Function that saves all provided notes
+     */
+    private void saveNotes() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            out.writeObject(notes);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    /**
+     * A function that implements the unloading of all records from the memory
+     * @return HashMap of identifications and notes
+     */
+    @SuppressWarnings("unchecked")
+    private HashMap<Integer, Note> loadNotes() {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            return (HashMap<Integer, Note>) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            log.error(e.getMessage());
+            return new HashMap<>();
+        }
+    }
+
+
+    private Note findNoteByTitle(String title) {
+        return null;
+    }
+
+    private String printNotes() {
         return null;
     }
 }
